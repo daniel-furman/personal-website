978187160de453fce2712398a2452a773b245fc4
daniel-furman <dryanfurman@gmail.com>
2025-07-23 23:09:40 -0700

    change blog page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -45,8 +45,8 @@ export default async function ArticlesIndex() {
 
   return (
     <SimpleLayout
-      title="Writing on technology, intermixed with a variety of other intellectual musings."
-      intro="All of my medium to long-form thoughts, collected in chronological order."
+      title="Intellectual musings"
+      intro="My medium to long-form thoughts, collected in chronological order."
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
