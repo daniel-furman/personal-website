52ae38a9ba0a11059fab2f931004a7e0fb3e7692
daniel-furman <dryanfurman@gmail.com>
2025-07-23 23:12:20 -0700

    change blog page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -37,7 +37,7 @@ function Article({ article }: { article: ArticleWithSlug }) {
 export const metadata: Metadata = {
   title: 'Articles',
   description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+    'My medium to long-form thoughts on technology and life, collected in chronological order.',
 }
 
 export default async function ArticlesIndex() {
@@ -45,8 +45,8 @@ export default async function ArticlesIndex() {
 
   return (
     <SimpleLayout
-      title="Intellectual musings"
-      intro="My medium to long-form thoughts, collected in chronological order."
+      title="Intellectual musings."
+      intro="My medium to long-form thoughts on technology and life, collected in chronological order."
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
