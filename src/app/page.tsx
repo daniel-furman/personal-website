6d69b3ee279daed0cd4f49da54a0e3a3f5420148
daniel-furman <dryanfurman@gmail.com>
2025-07-25 22:33:09 -0700

    latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -382,7 +382,9 @@ export default async function Home() {
             Not an AI.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-              👋 there! My name’s Daniel. I work as a data scientist in San Francisco and am focused on building GenAI products that customers value. These days, I get excited about contributing to the AI revolution from its epicenter, the San Francisco Bay Area. To get in touch, drop me a line at <a href="mailto:dryanfurman@gmail.com" className="text-zinc-800 underline decoration-zinc-300 underline-offset-2 hover:decoration-zinc-500 dark:text-zinc-100 dark:decoration-zinc-600 dark:hover:decoration-zinc-400">dryanfurman@gmail.com</a>.
+              👋 there! My name’s Daniel. I work as a data scientist in San Francisco and am focused on building GenAI products that customers value. These days, I get excited about contributing to the AI revolution from its epicenter, the San Francisco Bay Area. 
+              <br></br><br></br>
+              To get in touch, drop me a line at <a href="mailto:dryanfurman@gmail.com" className="text-zinc-800 underline decoration-zinc-300 underline-offset-2 hover:decoration-zinc-500 dark:text-zinc-100 dark:decoration-zinc-600 dark:hover:decoration-zinc-400">dryanfurman@gmail.com</a>.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink href="https://x.com/dryanfurman" target="_blank" rel="noopener noreferrer" aria-label="Follow on X" icon={XIcon} />
