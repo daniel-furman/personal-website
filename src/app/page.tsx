7742011fd359fc43d2618e583d726f796ce5f135
daniel-furman <dryanfurman@gmail.com>
2025-07-25 18:16:14 -0700

    scrolling for photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -268,15 +268,15 @@ function Photos() {
 
   return (
     <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+      <div className="flex overflow-x-auto gap-5 py-4 px-4 sm:gap-8 sm:px-8 scrollbar-hide">
         {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
           <div
             key={image.src}
-            className="flex flex-col items-center"
+            className="flex flex-col items-center flex-shrink-0"
           >
             <div
               className={clsx(
-                'relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
+                'relative aspect-9/10 w-44 overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
                 rotations[imageIndex % rotations.length],
               )}
             >
